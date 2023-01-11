60865dfae6f8e38066ea6e874f1b6cdf28f49ba9
MatthewM5293 <mmunozramirez@student.neumont.edu>
2023-01-10 22:01:05 -0700

    Added "AI Agent"

--- Assets/Scripts/Agent/Perception.cs
+++ Assets/Scripts/Agent/Perception.cs
@@ -7,6 +7,8 @@ public class Perception : MonoBehaviour
     [Range(1, 40)] public float distance = 1;
     [Range(0,180)] public float maxAngle = 45;
 
+    public string tagName = "";
+
     public GameObject[] GetGameObjects()
     {
         List<GameObject> result = new List<GameObject>();
@@ -18,7 +20,24 @@ public class Perception : MonoBehaviour
             {
                 continue;
             }
-            result.Add(collider.gameObject);
+
+            if (tagName == "" || collider.CompareTag(tagName))
+            {
+                result.Add(collider.gameObject);
+            }
+
+            // calculate angle from transform forward vector to direction of game object
+            Vector3 direction = (collider.transform.position - transform.position).normalized;
+
+            float cos = Vector3.Dot(transform.forward, direction);
+
+            float angle = Mathf.Acos(cos) * Mathf.Rad2Deg;
+
+            if (angle <= maxAngle)
+            {
+                result.Add(collider.gameObject);
+            }
+
         }
         return result.ToArray();
     }
