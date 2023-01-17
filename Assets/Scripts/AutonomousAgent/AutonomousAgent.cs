bd8c7cfcdf59dad77fc402047af1ba143a08ee1b
MatthewM5293 <mmunozramirez@student.neumont.edu>
2023-01-17 11:25:37 -0700

    Quick push for some modified files

--- Assets/Scripts/AutonomousAgent/AutonomousAgent.cs
+++ Assets/Scripts/AutonomousAgent/AutonomousAgent.cs
@@ -7,19 +7,8 @@ using UnityEngine;
 public class AutonomousAgent : Agent
 {
     public Perception flockPerception;
+    public AutonomousAgentData data;
 
-    [Range(0,3)]public float fleeWeight;
-    [Range(0,3)]public float seekWeight;
-
-    [Range(0,3)]public float cohesionWeight;
-    [Range(0,3)]public float separationWeight;
-    [Range(0,3)]public float alignmentWeight;
-
-    [Range(0,3)]public float separationRadius;
-
-    public float wanderDistance = 1;
-    public float wanderRadius = 3;
-    public float wanderDisplacement = 5;
     public float wanderAngle { get; set; } = 0;
 
     void Update()
@@ -31,16 +20,16 @@ public class AutonomousAgent : Agent
         }
         if (gameObjects.Length > 0)
         {
-            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * seekWeight);
-            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * fleeWeight);
+            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * data.seekWeight);
+            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * data.fleeWeight);
         }
 
         gameObjects = flockPerception.GetGameObjects();
         if (gameObjects.Length > 0)
         {
-            movement.ApplyForce(Steering.Cohesion(this, gameObjects) * cohesionWeight);
-            movement.ApplyForce(Steering.Separation(this, gameObjects, separationRadius) * separationWeight);
-            movement.ApplyForce(Steering.Alignment(this, gameObjects) * alignmentWeight);
+            movement.ApplyForce(Steering.Cohesion(this, gameObjects) * data.cohesionWeight);
+            movement.ApplyForce(Steering.Separation(this, gameObjects, data.separationRadius) * data.separationWeight);
+            movement.ApplyForce(Steering.Alignment(this, gameObjects) * data.alignmentWeight);
         }
 
         if (movement.acceleration.sqrMagnitude <= movement.maxForce * 0.1f)
@@ -48,7 +37,7 @@ public class AutonomousAgent : Agent
             movement.ApplyForce(Steering.Wander(this));
         }
 
-        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-20, -20, -20), new Vector3(20, 20, 20));
     }
 
 }
