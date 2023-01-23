bfcb5a41cc9c0a3f34eae2103dca87224a7fe485
MatthewM5293 <mmunozramirez@student.neumont.edu>
2023-01-23 10:56:15 -0700

    1/23/2023 Push
    Added ability to add Nodes while in Scene view

--- Assets/Scripts/Agent/NavNode.cs
+++ Assets/Scripts/Agent/NavNode.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class NavNode : MonoBehaviour
 {
+    [SerializeField] public List<NavNode> neighbors = new List<NavNode>();
+    [SerializeField, Range(1, 10)] public float radius = 1;
+    private void OnValidate()
+    {
+        GetComponent<SphereCollider>().radius = radius;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+        Gizmos.color = Color.green;
+        foreach (NavNode node in neighbors)
+        {
+            Gizmos.DrawLine(transform.position, node.transform.position);
+        }
+    }
+
+
     public static NavNode[] GetNodes()
     {
         return FindObjectsOfType<NavNode>();
